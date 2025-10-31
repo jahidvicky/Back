4ed839abb9f5d20000c6868e3718ddbfce7c9686
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-10-31 15:32:08 +0530

    mrge and update

--- index.js
+++ index.js
@@ -43,11 +43,11 @@ const app = express();
 
 // Allowed origins
 const allowedOrigins = [
-  // "http://localhost:5173",
-  // "http://localhost:5174",
-  "https://ataloptical.org",
-  "https://www.ataloptical.org",
-  "https://dashboard.ataloptical.org"
+  "http://localhost:5173",
+  "http://localhost:5174",
+  // "https://ataloptical.org",
+  // "https://www.ataloptical.org",
+  // "https://dashboard.ataloptical.org"
 
 ];
 
