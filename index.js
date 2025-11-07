c1bf86b81abc0ba728c6682aaf1855d35c186200
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-11-07 17:22:11 +0530

    merge and update

--- index.js
+++ index.js
@@ -45,11 +45,11 @@ const app = express();
 
 // Allowed origins
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  // "https://ataloptical.org",
-  // "https://www.ataloptical.org",
-  // "https://dashboard.ataloptical.org"
+  // "http://localhost:5173",
+  // "http://localhost:5174",
+  "https://ataloptical.org",
+  "https://www.ataloptical.org",
+  "https://dashboard.ataloptical.org"
 ];
 
 // CORS middleware
