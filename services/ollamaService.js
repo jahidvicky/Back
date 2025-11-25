0d0ab983a30ef82bae9098602dce60744d337658
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-11-25 17:12:05 +0530

    i ollama

--- services/ollamaService.js
+++ services/ollamaService.js
@@ -1,5 +1,5 @@
 const generateResponse = async (message) => {
-    const response = await fetch("http://localhost:11434/api/generate", {
+    const response = await fetch("http://127.0.0.1:11434/api/generate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
