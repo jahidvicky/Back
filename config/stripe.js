cd2f524b508feefd8efd8c9074116d85dcd1dcac
Dipanshu <dipanshu.patel.wwl@gmail.com>
2026-01-28 12:39:56 +0530

    fix payment

--- config/stripe.js
+++ config/stripe.js
@@ -1,4 +1,20 @@
+// const Stripe = require("stripe");
+
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+//     apiVersion: "2023-10-16",
+// });
+
+// module.exports = stripe;
+
+
+
+
 const Stripe = require("stripe");
+
+if (!process.env.STRIPE_SECRET_KEY) {
+    console.error("STRIPE_SECRET_KEY is missing");
+    process.exit(1);
+}
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2023-10-16",
