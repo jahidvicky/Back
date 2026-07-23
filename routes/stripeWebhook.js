4e375a6183c41170b47fd5f841b9e133721b6b1a
jahidvicky <jahidvicky@gmail.com>
2026-07-23 18:04:33 +0530

    fix webhook stripe route

--- routes/stripeWebhook.js
+++ routes/stripeWebhook.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { handleStripeWebhook } = require("../controller/stripeWebhook");
 
 router.post(
-    "/webhooks/stripe",
+    "/",
     express.raw({ type: "application/json" }),
     handleStripeWebhook
 );
